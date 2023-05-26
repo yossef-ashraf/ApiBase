c26e08fbc0536574560b3cd9250d869e861252b0
yossef-ashraf <yossefff2001@gmail.com>
2023-05-27 01:41:04 +0300

    add some Packages to help

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        // Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
 ];
